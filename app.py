3a0ed5bc10aa4ab2ee9234935d9b9562d2d057c2
youbaAg <youba.aguenana.edu@groupe-gema.com>
2023-01-27 17:36:39 +0100

    add serve

--- app.py
+++ app.py
@@ -160,4 +160,5 @@ def prediction():
     return render_template("prediction.html", results = results)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
