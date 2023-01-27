59b9ffe3a7b0a8356752974e37dd49e088f71a9c
youbaAg <youba.aguenana.edu@groupe-gema.com>
2023-01-27 18:48:47 +0100

    delete if main

--- app.py
+++ app.py
@@ -158,7 +158,3 @@ def prediction():
         results = route__knn()
 
     return render_template("prediction.html", results = results)
-
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
